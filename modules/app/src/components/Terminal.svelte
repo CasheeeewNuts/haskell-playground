d208c20be1880bed0fc0af43c71f2160dc5c1f68
CasheeeewNuts <casheeeewnuts+tech@gmail.com>
2022-04-08 00:20:26 +0900

    add xterm

---
+++ modules/app/src/components/Terminal.svelte
@@ -0,0 +1,16 @@
+<script>
+    import {onMount} from "svelte";
+    import {Terminal} from "xterm";
+    import "xterm/css/xterm.css"
+
+    const terminal = new Terminal()
+    let terminalRef = null
+
+    onMount(() => {
+        if (terminalRef) {
+            terminal.open(terminalRef)
+        }
+    })
+</script>
+
+<div bind:this={terminalRef} class=""></div>
